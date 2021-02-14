e2b75db1da8cbb5f75792aae5f042d48205881ad
Marlon Strong <tsenane14@gmail.com>
2021-02-14 17:47:20 +0200

    added routes at /App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import GlobalStyle from './Pages/styles/global'
 
@@ -11,7 +12,11 @@ const [vis,setvis]=useState(false);
 
   return (
     <>
-      <Homepage ip={ip} setip={setip} setvis={setvis} vis={vis} />
+    <BrowserRouter basename="/ipaddresstracker">
+      <Route path="/" exact>
+        <Homepage ip={ip} setip={setip} setvis={setvis} vis={vis} />
+      </Route>
+    </BrowserRouter>
       <GlobalStyle></GlobalStyle>
     </>
   );
