17484092cf2f916bcb9f84c6d46ed9bad6f827b7
Marlon Strong <tsenane14@gmail.com>
2021-02-14 17:36:32 +0200

    aadding gh-pages to deploy

---
+++ src/Pages/Homepage/HomepageElements.js
@@ -0,0 +1,247 @@
+import styled from "styled-components";
+import {IoIosArrowForward} from 'react-icons/io';
+
+
+export const BgContainer=styled.div`
+z-index:1;
+
+img{
+    min-width:100%;
+    /* height:100%; */
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    
+    /* background:white url(bg) no-repeat fixed center; */
+}
+
+iframe{
+    position:absolute;
+    //now it fits right
+    object-fit:cover;
+    max-width:100vw;
+    height:72vh;
+    }
+
+`
+
+export const Container=styled.div`
+z-index: 2;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+
+
+
+h1{
+    position:absolute;
+    z-index:2;
+    color:var(--white);
+    font-size:30px;
+    font-weight:500;
+    margin-bottom: 400px;
+
+}
+form{
+    
+    display:flex;
+   
+    justify-content:center;
+    align-items:center;
+   
+
+    @media (max-width:600px){
+         
+    }
+}
+
+
+
+.divButton{
+    position:absolute;
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    width:35vw;
+    height: 40px;
+    margin-bottom: 280px;
+    padding:10px;
+
+    @media (max-width:600px){
+         
+    }
+
+    input{
+        min-width:35vw;
+        text-indent:10px;
+    display:flex;
+    flex:1;    
+    background:var(--white);
+    border-radius:9px 0 0 9px;
+    color:var(--dark-gray);
+    font-size:18px;
+    outline:none;
+
+    min-height:50px;
+    border:${(vis)=>vis?'none':'2px solid red'};
+    }
+    button{
+        min-height:50px;
+        min-width:40px;
+        background:var(--dark-gray);
+        border-radius:0px 9px 9px 0;
+        outline:none;
+        border:1px solid var(--dark-gray);
+        cursor: pointer;
+        padding:2px;
+        transition:all 0.2s ease-in-out;
+        &:hover{
+                background:var(--very-dark-gray);
+        }
+
+        
+             
+
+
+    }
+}
+.divData{
+    position:absolute;
+    flex-direction:row;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    width:70vw;
+    min-height:15vh;
+    background:var(--white);
+    border-radius:10px;
+    padding-left:10px;
+    box-shadow: 0px 4px 18px rgba(0,0,0,0.2);
+
+    transition:all 0.2s ease-in-out;
+
+    @media (max-width:1000px){
+        flex-direction:column;
+        margin-top:30vh;
+        width:35vw;
+        
+        
+    }
+    @media (max-width:600px){
+         width:70vw;
+         height:40vh; 
+         padding:0;
+         margin-top:150px;
+         justify-content:center;
+         align-items:center;
+         text-align:center;
+         padding:0;
+
+         
+         
+    }
+
+    h1{
+        margin-left:260px;
+        margin-top:150px;
+        visibility:${(vis)=>vis?'hidden':'visible'};
+    }
+
+    span{
+        width:13vw; //20vw works fine
+        
+        /* height:20vh; */
+        padding:10px;
+        padding-left:20px;
+        align-items:center;
+        /* flex:0 auto; */
+        border-left:0.2px solid rgba(0,0,0,0.2);
+        border-spacing:0px;
+        
+        /* ::after{
+            content:"";
+            position:absolute; 
+            bottom:90px;
+            border-right:2px solid gray;
+        } */
+
+        &+span{
+            padding-right:30px;
+        }
+        h3{
+            
+            color:var(--dark-gray);
+            font-size:14px;
+            font-weight:400;
+            padding-bottom:10px;
+
+            @media (max-width:600px){
+            width:70vw;
+            /* height:10vh; */
+            margin:0; 
+            text-align:center;
+
+    }
+        }
+        h2{
+            min-height:56px;
+            color:var(--very-dark-gray);
+            font-size:22px;
+            transition:all 0.2s ease-in-out; 
+         
+            @media (max-width:600px){
+            width:70vw;
+            height:10vh;
+            margin:0; 
+            text-align:center;
+        
+         
+        
+            }
+        }
+        @media (max-width:1000px){
+            border:none;
+            /* width:35vw; */
+           
+        }
+        @media (max-width:600px){
+            width:70vw;
+            height:10vh;
+            margin:0; 
+            text-align:center;
+            padding:0px;
+            padding-top:10px;
+         
+        
+    }
+        
+        
+    }.corner{
+            border-left:none;
+            /* border-right:none; */
+            /* background:red; */
+        }
+}
+
+
+`
+export const Arrow=styled(IoIosArrowForward)`
+width:16px;
+height:16px;
+fill:var(--white);
+
+
+
+`
+export const AlertS=styled.h1`
+position:absolute;
+z-index:5;
+color:red;
+font-size:40px;
+padding:0;
+margin:0;
+margin-left:150px;
+`
+//comit to commit
---
+++ src/Pages/Homepage/index.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+
+import bg from '../../Assets/pattern-bg.png';
+import api from '../../services/api';
+import { abz} from '../../myapi';
+
+import { Arrow, BgContainer, Container, AlertS } from './HomepageElements';
+
+require('dotenv').config();
+
+
+const API_KEY=process.env.REACT_APP_GOOGLE_API_KEY;
+
+const ipData={
+    ip: String,
+    city: String,
+    countrycode:String,
+    timezone:String,
+    isp:String
+}
+
+function Homepage({ip, setip, vis, setvis}) {
+const [ips,setips]=useState([]);
+const [input,setinput]=useState();
+
+// useEffect(() => {
+//     if(ips.status==="fail"){
+//         alert("write a valid ip")
+//     }
+    
+// }, [])
+
+
+
+
+    
+    
+
+  
+    const handleIpData=e=>{
+       
+            setip(e.target.value);
+            console.log(ip);
+            if(ip===""){
+                setvis(false);
+            }
+        
+    }
+  
+
+  async  function handleSubmit(event){
+        if(event){
+            event.preventDefault();
+           
+            const response=await api.get(`/${ip}?fields=country,countryCode,city,offset,isp,query,lat,lon,regionName,status`);
+            const allip=response.data;
+            setips(allip);
+            setinput(ip);
+            setip("");
+            setvis(true);
+            console.log(ip);
+            console.log(vis);
+            console.log(response.data);
+            console.log(ips.lat);
+            
+        } 
+    
+    } 
+   
+    return (
+        <>
+        <BgContainer>
+            <img src={bg} alt="abstract"/>
+           
+            
+            
+            {/* Google map without icon */}
+            <iframe
+  width="1940"
+  height="650"
+  frameBorder="0" style={{border:0}}
+  src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}
+  &q=${ips.city}+","+${ips.regionName}+","+${ips.country}`} allowFullScreen>
+</iframe>
+
+            
+            
+            <Container>
+            <h1>IP Address Tracker</h1>
+            <div className="divButton">
+                    <form onSubmit={handleSubmit}>
+                    <input onChange={handleIpData} value={ip} vis={vis}  placeholder="press enter to use your ip"/>
+                    <button type="submit"   ><Arrow/></button>
+                    
+                    
+                    </form>
+                    
+                 
+            </div>
+            <div className="divData">
+            <h1 style={{color:"red",fontSize:"11px" }}>Write IP. Ex: 8.8.8.8</h1> {/* continua */}
+            
+                <span className="corner">
+                    <h3>IP ADDRESS</h3>
+                    <h2>{ips.status==="success"?ips.query:"Write valid ip"}</h2>
+                </span>
+                <span>
+                    <h3>LOCATION</h3>
+                    <h2>{ips.status==="success"?(ips.city+","+ips.countryCode):"-"}</h2>
+                </span>
+                <span>
+                    <h3>TIMEZONE</h3>
+                    <h2>{ips.status==="success"?("UTC"+" "+Math.round((ips.offset)/3600)+":00"):"-"}</h2>
+                </span>
+                <span >
+                    <h3>ISP</h3>
+                    <h2>{ips.status==="success"?ips.isp:"-"}</h2>
+                </span>
+            </div>
+
+        </Container>
+
+        </BgContainer>
+        
+
+        </>
+    )
+}
+
+export default Homepage
