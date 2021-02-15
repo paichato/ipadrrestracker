be74ddcd57f5d07dd63aa39c2a55e756c5a27049
Marlon Strong <tsenane14@gmail.com>
2021-02-15 02:38:21 +0200

    changing ip-api to ipfy

--- src/Pages/Homepage/index.js
+++ src/Pages/Homepage/index.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 
 const API_KEY=process.env.REACT_APP_GOOGLE_API_KEY;
+export const IPFY_API_KEY=process.env.REACT_APP_IPFY_API_KEY;
 
 const ipData={
     ip: String,
