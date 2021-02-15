8ed75a306ca551e92d30bf86b8278c6c75a95e1b
Marlon Strong <tsenane14@gmail.com>
2021-02-15 16:11:39 +0200

    sync loader to load after 3 sec

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import MainLoader from './Pages/MainLoader';
 import GlobalStyle from './Pages/styles/global'
 
+const useExpired = (time)=>{
+  const [expired, setExpired] = useState(false);
+  const timoutRef = useRef();
+  useEffect(()=>{
+    timoutRef.current = setTimeout(()=>{
+      setExpired(true);
+    }, time);
+    return ()=>{
+      clearTimeout(timoutRef.current);
+    }
+  },[time]);
+  return expired;
+}
+
+
 function App() {
   
 const [ip, setip] = useState("");
 const [vis,setvis]=useState(false);
-
+const expired=useExpired(3000);
 
 
   return (
     <>
+    {expired?(
+      <>
     <BrowserRouter basename="/ipadrrestracker">
       <Route path="/" exact>
         <Homepage ip={ip} setip={setip} setvis={setvis} vis={vis} />
       </Route>
       
     </BrowserRouter>
-    <MainLoader/>
-      <GlobalStyle></GlobalStyle>
+    
+      <GlobalStyle/>
+      </>):<MainLoader/>}
     </>
   );
 }
