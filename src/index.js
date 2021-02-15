5eb108b3834ca9b56809c273af498d574aab3356
Marlon Strong <tsenane14@gmail.com>
2021-02-15 21:57:26 +0200

    favicon added sucessfuly

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url="https://github.com/paichato/ipadrrestracker/blob/main/mascardevs%20favicon.png?raw=true"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
