00184d0c5fa733240d3abcc57c46ce56360ebb7c
Marlon Strong <tsenane14@gmail.com>
2021-02-15 02:40:35 +0200

    changed axios baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import IPFY_API_KEY from '../Pages/Homepage/index';
 
 
 const api=axios.create({
-    baseURL: 'https://ip-api.com/json'
+    baseURL: `https://geo.ipify.org/api/v1?apiKey=${IPFY_API_KEY}`
 })
 
 export default api;
