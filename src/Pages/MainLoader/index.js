a49e463998251fc0f47bca5c43faf70d2e32763f
Marlon Strong <tsenane14@gmail.com>
2021-02-15 14:05:20 +0200

    cleated index.js content

---
+++ src/Pages/MainLoader/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MainLoader() {
+    return (
+        <>
+        <MainLoaderContainer>
+            <h1></h1>
+            <hr/>
+        </MainLoaderContainer>
+            
+        </>
+    )
+}
+
+export default MainLoader
